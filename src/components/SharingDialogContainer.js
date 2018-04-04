07c428155a9783fcf34840a085e1ef61555c792b
Hendrik de Graaf <hendrik@dhis2.org>
2018-04-04 11:58:47 +0200

    Simplified SharingDialogContainer because there is no way to check for changes in sharing settings anyway

--- src/components/SharingDialogContainer.js
+++ src/components/SharingDialogContainer.js
@@ -1,32 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import i18next from 'i18next';
 import { connect } from 'react-redux';
 import SharingDialog from 'd2-ui-sharing';
 import api from '../api';
 import { hideSharingDialog, showSnackbar } from '../actions';
 
-class SharingDialogContainer extends Component {
-    closeAndConfirm = () => {
-        const { hideSharingDialog, showSnackbar } = this.props;
-        const message = i18next.t('Sharing settings have been updated');
-        hideSharingDialog();
-        showSnackbar({ message });
-    };
-
-    render() {
-        const { open, id, type } = this.props;
-        return (
-            <SharingDialog
-                open={open}
-                id={id}
-                type={type}
-                onRequestClose={this.closeAndConfirm}
-                d2={api.getD2()}
-            />
-        );
-    }
-}
+const SharingDialogContainer = ({ open, id, type, hideSharingDialog }) => {
+    return (
+        <SharingDialog
+            open={open}
+            id={id}
+            type={type}
+            onRequestClose={hideSharingDialog}
+            d2={api.getD2()}
+        />
+    );
+};
 
 SharingDialogContainer.propTypes = {
     open: PropTypes.bool.isRequired,
