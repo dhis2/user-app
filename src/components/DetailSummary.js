2908126b9d2a18b43b0063f11a29e04d2e4a586b
Hendrik de Graaf <hendrik@dhis2.org>
2018-04-18 16:12:47 +0200

    Slimmed down the RoleDetails/ GroupDetails/ UserProfile component: - Refactored DetailSummary into a class based component that can fetch it's own data - Refactored the RoleDetails/ GroupDetails/ UserProfile components into functional components

--- src/components/DetailSummary.js
+++ src/components/DetailSummary.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Paper } from 'material-ui';
 import _ from '../constants/lodash';
@@ -13,7 +13,9 @@ import RaisedButton from 'material-ui/RaisedButton';
 import ImageEdit from 'material-ui/svg-icons/image/edit';
 import ContentSend from 'material-ui/svg-icons/content/send';
 import { Link } from 'react-router-dom';
-import { USER } from '../constants/entityTypes';
+import { USER, DETAILS } from '../constants/entityTypes';
+import { connect } from 'react-redux';
+import { getItem } from '../actions';
 
 const styles = {
     main: {
@@ -43,133 +45,157 @@ const styles = {
     },
 };
 
-const renderPropertyFields = (summaryObject, config) => {
-    const labelCellStyle = { ...styles.cell, ...styles.valueCell };
-    return config.map((field, index) => {
-        let {
-            key,
-            label,
-            removeText,
-            parseDate,
-            nestedPropselector,
-            parseArrayAsCommaDelimitedString,
-            count,
-        } = field;
-        label = i18n.t(label);
-        let value = summaryObject[key];
+class DetailSummary extends Component {
+    componentWillMount() {
+        const { baseName, routeId, getItem } = this.props;
+        getItem(baseName, DETAILS, routeId);
+    }
 
-        if (typeof value === 'undefined') {
-            value = '';
-        } else {
-            if (nestedPropselector) {
-                nestedPropselector.forEach(selector => {
-                    value = value[selector];
-                });
-            }
+    renderSendMessageBtn(userId) {
+        const url = `${api.getContextPath()}/dhis-web-messaging/showSendMessage.action?id=${userId}`;
+        return (
+            <RaisedButton
+                style={styles.raisedButton}
+                label={i18n.t('Send message')}
+                secondary={true}
+                containerElement={<a href={url}> </a>}
+                icon={<ContentSend />}
+            />
+        );
+    }
 
-            if (parseArrayAsCommaDelimitedString) {
-                // Some nested lists come through as a modelCollection but others are already arrays
-                if (typeof value.toArray === 'function') {
-                    value = value.toArray();
+    renderPropertyFields() {
+        const { summaryObject, config } = this.props;
+        const labelCellStyle = { ...styles.cell, ...styles.valueCell };
+
+        return config.map((field, index) => {
+            let {
+                key,
+                label,
+                removeText,
+                parseDate,
+                nestedPropselector,
+                parseArrayAsCommaDelimitedString,
+                count,
+            } = field;
+            label = i18n.t(label);
+            let value = summaryObject[key];
+
+            if (typeof value === 'undefined') {
+                value = '';
+            } else {
+                if (nestedPropselector) {
+                    nestedPropselector.forEach(selector => {
+                        value = value[selector];
+                    });
                 }
-                value = value
-                    .map(item => item[parseArrayAsCommaDelimitedString])
-                    .join(', ');
-            }
 
-            if (removeText) {
-                value = _.capitalize(value.replace(removeText, ''));
-            }
+                if (parseArrayAsCommaDelimitedString) {
+                    // Some nested lists come through as a modelCollection but others are already arrays
+                    if (typeof value.toArray === 'function') {
+                        value = value.toArray();
+                    }
+                    value = value
+                        .map(item => item[parseArrayAsCommaDelimitedString])
+                        .join(', ');
+                }
 
-            if (parseDate && typeof value === 'string') {
-                value = parseDateFromUTCString(value);
-            }
+                if (removeText) {
+                    value = _.capitalize(value.replace(removeText, ''));
+                }
 
-            if (count) {
-                if (typeof value.size === 'number') {
-                    value = value.size;
-                } else if (typeof value.length === 'number') {
-                    value = value.length;
+                if (parseDate && typeof value === 'string') {
+                    value = parseDateFromUTCString(value);
+                }
+
+                if (count) {
+                    if (typeof value.size === 'number') {
+                        value = value.size;
+                    } else if (typeof value.length === 'number') {
+                        value = value.length;
+                    }
                 }
             }
+
+            return (
+                <tr key={index}>
+                    <td style={labelCellStyle}>{label}</td>
+                    <td style={styles.cell}>{value}</td>
+                </tr>
+            );
+        });
+    }
+
+    render() {
+        const { summaryObject, baseName } = this.props;
+
+        if (summaryObject === null) {
+            return <LoadingMask />;
         }
 
-        return (
-            <tr key={index}>
-                <td style={labelCellStyle}>{label}</td>
-                <td style={styles.cell}>{value}</td>
-            </tr>
-        );
-    });
-};
-
-const renderSendMessageBtn = userId => {
-    const url = `${api.getContextPath()}/dhis-web-messaging/showSendMessage.action?id=${userId}`;
-    return (
-        <RaisedButton
-            style={styles.raisedButton}
-            label={i18n.t('Send message')}
-            secondary={true}
-            containerElement={<a href={url}> </a>}
-            icon={<ContentSend />}
-        />
-    );
-};
-
-const DetailSummary = ({ summaryObject, config, baseName }) => {
-    if (summaryObject === null) {
-        return <LoadingMask />;
-    }
-
-    if (typeof summaryObject === 'string') {
-        const errorText = i18n.t(`There was an error fetching the ${baseName}`);
-        return (
-            <ErrorMessage introText={errorText} errorMessage={summaryObject} />
-        );
-    }
-
-    const { id, displayName, access } = summaryObject;
-    const plural = `${baseName}s`,
-        baseRoute = `/${_.kebabCase(plural)}`,
-        backTooltip = i18n.t(`Back to ${plural}`),
-        editLink = `${baseRoute}/edit/${id}`,
-        editTooltip = i18n.t(`Edit ${baseName}`);
-
-    return (
-        <main style={styles.main}>
-            <Heading style={styles.heading}>
-                <IconLink
-                    to={baseRoute}
-                    tooltip={backTooltip}
-                    icon="arrow_back"
+        if (typeof summaryObject === 'string') {
+            const errorText = i18n.t(
+                `There was an error fetching the ${baseName}`
+            );
+            return (
+                <ErrorMessage
+                    introText={errorText}
+                    errorMessage={summaryObject}
                 />
-                {displayName}
+            );
+        }
 
-                {access.update ? (
-                    <RaisedButton
-                        style={styles.raisedButton}
-                        label={editTooltip}
-                        primary={true}
-                        containerElement={<Link to={editLink} />}
-                        icon={<ImageEdit />}
+        const { id, displayName, access } = summaryObject;
+        const plural = `${baseName}s`,
+            baseRoute = `/${_.kebabCase(plural)}`,
+            backTooltip = i18n.t(`Back to ${plural}`),
+            editLink = `${baseRoute}/edit/${id}`,
+            editTooltip = i18n.t(`Edit ${baseName}`);
+
+        return (
+            <main style={styles.main}>
+                <Heading style={styles.heading}>
+                    <IconLink
+                        to={baseRoute}
+                        tooltip={backTooltip}
+                        icon="arrow_back"
                     />
-                ) : null}
+                    {displayName}
 
-                {baseName === USER ? renderSendMessageBtn(id) : null}
-            </Heading>
-            <Paper style={styles.paper}>
-                <table>
-                    <tbody>{renderPropertyFields(summaryObject, config)}</tbody>
-                </table>
-            </Paper>
-        </main>
-    );
-};
+                    {access.update ? (
+                        <RaisedButton
+                            style={styles.raisedButton}
+                            label={editTooltip}
+                            primary={true}
+                            containerElement={<Link to={editLink} />}
+                            icon={<ImageEdit />}
+                        />
+                    ) : null}
+
+                    {baseName === USER ? this.renderSendMessageBtn(id) : null}
+                </Heading>
+                <Paper style={styles.paper}>
+                    <table>
+                        <tbody>{this.renderPropertyFields()}</tbody>
+                    </table>
+                </Paper>
+            </main>
+        );
+    }
+}
 
 DetailSummary.propTypes = {
     summaryObject: PropTypes.object,
+    routeId: PropTypes.string.isRequired,
     config: PropTypes.array.isRequired,
     baseName: PropTypes.string.isRequired,
+    getItem: PropTypes.func.isRequired,
 };
 
-export default DetailSummary;
+const mapStateToProps = state => ({
+    summaryObject: state.currentItem,
+});
+
+export default connect(mapStateToProps, {
+    getItem,
+})(DetailSummary);
--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -1,41 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { USER, DETAILS } from '../constants/entityTypes';
+import { USER } from '../constants/entityTypes';
 import DetailSummary from '../components/DetailSummary';
-import { connect } from 'react-redux';
 import { USER_PROFILE } from '../constants/detailFieldConfigs';
-import { getItem } from '../actions';
 
-class UserProfile extends Component {
-    componentWillMount() {
-        const { getItem, match: { params: { id } } } = this.props;
-        getItem(USER, DETAILS, id);
-    }
-
-    render() {
-        const { user } = this.props;
-        return (
-            <DetailSummary
-                summaryObject={user}
-                config={USER_PROFILE}
-                baseName={USER}
-            />
-        );
-    }
-}
+const UserProfile = ({ match: { params: { id } } }) => (
+    <DetailSummary routeId={id} config={USER_PROFILE} baseName={USER} />
+);
 
 UserProfile.propTypes = {
     match: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
-    user: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-    getItem: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-    user: state.currentItem,
-});
-
-export default connect(mapStateToProps, {
-    getItem,
-})(UserProfile);
+export default UserProfile;
