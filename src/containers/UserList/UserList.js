30199c4effb43690016ab35ffd8d8ec5b333a65d
Hendrik de Graaf <hendrik@dhis2.org>
2018-04-18 16:20:41 +0200

    Refactored RoleList and UserList into stateless components (GroupList relies on a lifecycle method so needs to stay a component)

--- src/containers/UserList/UserList.js
+++ src/containers/UserList/UserList.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { USER } from '../../constants/entityTypes';
 import List from '../../components/List';
 import i18n from 'd2-i18n';
@@ -11,29 +10,19 @@ import {
 } from './UserContextMenuActions';
 import UserFilter from './UserFilter';
 
-class UserList extends Component {
-    render() {
-        return (
-            <List
-                entityType={USER}
-                columns={['displayName', 'userName', 'disabled']}
-                FilterComponent={UserFilter}
-                primaryAction={userContextMenuActions.edit}
-                contextMenuActions={userContextMenuActions}
-                contextMenuIcons={userContextMenuIcons}
-                isContextActionAllowed={isUserContextActionAllowed}
-                sectionName={i18n.t('User Management')}
-                newItemPath={'/users/new'}
-                className="user-list"
-            />
-        );
-    }
-}
-
-UserList.propTypes = {
-    match: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
-};
+const UserList = () => (
+    <List
+        entityType={USER}
+        columns={['displayName', 'userName', 'disabled']}
+        FilterComponent={UserFilter}
+        primaryAction={userContextMenuActions.edit}
+        contextMenuActions={userContextMenuActions}
+        contextMenuIcons={userContextMenuIcons}
+        isContextActionAllowed={isUserContextActionAllowed}
+        sectionName={i18n.t('User Management')}
+        newItemPath={'/users/new'}
+        className="user-list"
+    />
+);
 
 export default UserList;
