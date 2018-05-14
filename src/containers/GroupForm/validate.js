cc0fbbfa18da4b131529ecaded5fec544a44de5d
Hendrik de Graaf <hendrik@dhis2.org>
2018-05-14 16:40:39 +0200

    Validate UserGroup code field length max 50 chars

--- src/containers/GroupForm/validate.js
+++ src/containers/GroupForm/validate.js
@@ -1,5 +1,5 @@
 import i18n from '@dhis2/d2-i18n';
-import { NAME, USERS } from './config';
+import { NAME, CODE, USERS } from './config';
 
 export default function validate(values, { pristine }) {
     let errors = {};
@@ -15,6 +15,10 @@ export default function validate(values, { pristine }) {
 
     if (values[USERS].length === 0) {
         errors[USERS] = requiredMsg;
+    }
+
+    if (values[CODE] && values[CODE].length > 50) {
+        errors[CODE] = i18n.t('Exceeds maximum character limit of 50');
     }
 
     return errors;
