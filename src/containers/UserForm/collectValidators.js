0ec7e1357385abd6df8d2a65658e4fbca7d86778
Hendrik de Graaf <hendrik@dhis2.org>
2018-12-06 09:33:32 +0100

    Added comments to clarify which user-fields are required in different scenarios

--- src/containers/UserForm/collectValidators.js
+++ src/containers/UserForm/collectValidators.js
@@ -14,6 +14,10 @@ const validatorLookup = {
     userRoles: validators.requiredArray,
 };
 
+// When creating a user, a password is required, because there is no step to create
+// a password, which a user does get when he is invited by email. On the other hand,
+// the email field is optional, because the user can just use his username to login.
+// And for this reason, the username field is required too.
 const CREATE_REQUIRED_FIELDS = new Set([
     USERNAME,
     PASSWORD,
@@ -21,7 +25,15 @@ const CREATE_REQUIRED_FIELDS = new Set([
     SURNAME,
     FIRST_NAME,
 ]);
+
+// When inviting a user, email, firstname and surname is sufficient.
+// The user can setup a username and password when clicking on the invite link.
 const INVITE_REQUIRED_FIELDS = new Set([EMAIL, SURNAME, FIRST_NAME]);
+
+// In edit mode, the username field is disabled, because this is create-only.
+// The password fields are also optional, we just keep the current password 
+// if no new values are provided. Only firstname and surname are required,
+// to prevent these fields from being cleared.
 const EDIT_REQUIRED_FIELDS = new Set([SURNAME, FIRST_NAME]);
 
 export default function collectValidators(
