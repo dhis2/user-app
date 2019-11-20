603e26926d5cf90cbd93f51de6f26d848969c8cd
Hendrik de Graaf <hendrik@dhis2.org>
2019-11-20 10:11:45 +0100

    fix(replicate-user): add autocomplete="new-password" to prevent autofill

--- src/components/ReplicateUserForm.js
+++ src/components/ReplicateUserForm.js
@@ -72,6 +72,7 @@ class ReplicateUserForm extends Component {
                     label={i18n.t('Username')}
                     hintText={i18n.t('Username for new user')}
                     validate={[username]}
+                    autoComplete="new-password"
                 />
                 <Field
                     name={PASSWORD}
@@ -80,6 +81,7 @@ class ReplicateUserForm extends Component {
                     hintText={i18n.t('Password for new user')}
                     validate={[password]}
                     type="password"
+                    autoComplete="new-password"
                 />
                 <div style={{ marginTop: 16 }}>
                     <RaisedButton
