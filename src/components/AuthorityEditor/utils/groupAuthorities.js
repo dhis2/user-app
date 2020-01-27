ac06b3bba3014d9ed6a20f80339f4f8534ccd993
Hendrik de Graaf <hendrik@dhis2.org>
2020-01-27 13:54:52 +0100

    fix(authorities): add visualization and remove report-table and chart

--- src/components/AuthorityEditor/utils/groupAuthorities.js
+++ src/components/AuthorityEditor/utils/groupAuthorities.js
@@ -71,12 +71,18 @@ const IMPLICIT_GROUP_ITEM = {
 
 // Metadata with implicit add and delete
 const AUTHS_WITH_IMPLICIT_ADD_PRIVATE_AND_DELETE = new Set([
-    'F_CHART_PUBLIC_ADD',
     'F_DASHBOARD_PUBLIC_ADD',
     'F_EVENTCHART_PUBLIC_ADD',
     'F_EVENTREPORT_PUBLIC_ADD',
     'F_MAP_PUBLIC_ADD',
+    'F_VISUALIZATION_PUBLIC_ADD',
+])
+
+const DEPRECATED_AUTHS = new Set([
+    'F_CHART_PUBLIC_ADD',
+    'F_CHART_EXTERNAL',
     'F_REPORTTABLE_PUBLIC_ADD',
+    'F_REPORTTABLE_EXTERNAL',
 ])
 
 const AUTHORITY_GROUPS = {
@@ -144,7 +150,9 @@ const AUTHORITY_GROUPS = {
 const groupAuthorities = authorities => {
     // A lookup map that can be used to verify the existence of a particular authority ID in linear time
     const lookup = authorities.reduce((lookup, auth) => {
-        lookup.set(auth.id, auth)
+        if (!DEPRECATED_AUTHS.has(auth.id)) {
+            lookup.set(auth.id, auth)
+        }
         return lookup
     }, new Map())
 
