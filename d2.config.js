21c8bd6a1abd85ec315876b317914f78099c5e91
Hendrik de Graaf <hendrik@dhis2.org>
2020-09-09 10:45:17 +0200

    fix: add app description to d2 config

--- d2.config.js
+++ d2.config.js
@@ -3,6 +3,8 @@ const config = {
     coreApp: true,
     title: 'User management',
     name: 'user',
+    description:
+        'An application for managing users, user-groups and user-roles',
     entryPoints: {
         app: './src/AppWrapper',
     },
