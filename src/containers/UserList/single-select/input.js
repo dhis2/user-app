8e2ed39ad41aea616d369acc4834bdff3d0e4ed0
Médi-Rémi Hashim <mediremi@gmail.com>
2021-10-15 10:55:51 +0100

    fix(single-select): take value prop

--- src/containers/UserList/single-select/input.js
+++ src/containers/UserList/single-select/input.js
@@ -1,89 +1,36 @@
-import { requiredIf } from '@dhis2/prop-types'
 import { colors } from '@dhis2/ui-constants'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {
-    InputClearButton,
-    InputPlaceholder,
-    InputPrefix,
-} from '../select/index.js'
-import { Selection } from './selection.js'
+import { InputPrefix } from '../select/index.js'
 
-const Input = ({
-    selected,
-    onChange,
-    clearable,
-    clearText,
-    placeholder,
-    dataTest,
-    prefix,
-    options,
-    className,
-    disabled,
-    inputMaxHeight,
-}) => {
-    const hasSelection = selected && typeof selected === 'string'
-    const onClear = (_, e) => {
-        const data = { selected: '' }
+const Input = ({ dataTest, prefix, className, inputMaxHeight, value }) => (
+    <div className={cx('root', className)}>
+        <InputPrefix prefix={prefix} dataTest={`${dataTest}-prefix`} />
+        <span>{value}</span>
 
-        e.stopPropagation()
-        onChange(data, e)
-    }
+        <style jsx>{`
+            .root {
+                display: flex;
+                align-items: center;
+                color: ${colors.grey900};
+                font-size: 14px;
+                line-height: 16px;
+            }
 
-    return (
-        <div className={cx('root', className)}>
-            <InputPrefix prefix={prefix} dataTest={`${dataTest}-prefix`} />
-            {!hasSelection && !prefix && (
-                <InputPlaceholder
-                    placeholder={placeholder}
-                    dataTest={`${dataTest}-placeholder`}
-                />
-            )}
-            {hasSelection && (
-                <div className="root-input">
-                    {/* the wrapper div above is necessary to enforce wrapping on overflow */}
-                    <Selection selected={selected} options={options} />
-                </div>
-            )}
-            {hasSelection && clearable && !disabled && (
-                <div className="root-right">
-                    <InputClearButton
-                        onClear={onClear}
-                        clearText={clearText}
-                        dataTest={`${dataTest}-clear`}
-                    />
-                </div>
-            )}
+            .root-input {
+                overflow-y: auto;
+                flex: 1;
+            }
+        `}</style>
 
-            <style jsx>{`
-                .root {
-                    display: flex;
-                    align-items: center;
-                    color: ${colors.grey900};
-                    font-size: 14px;
-                    line-height: 16px;
-                }
-
-                .root-input {
-                    overflow-y: auto;
-                    flex: 1;
-                }
-
-                .root-right {
-                    margin-left: auto;
-                    margin-right: 10px;
-                }
-            `}</style>
-
-            <style jsx>{`
-                .root-input {
-                    max-height: ${inputMaxHeight};
-                }
-            `}</style>
-        </div>
-    )
-}
+        <style jsx>{`
+            .root-input {
+                max-height: ${inputMaxHeight};
+            }
+        `}</style>
+    </div>
+)
 
 Input.defaultProps = {
     inputMaxHeight: '100px',
@@ -91,16 +38,10 @@ Input.defaultProps = {
 
 Input.propTypes = {
     className: PropTypes.string,
-    clearText: requiredIf(props => props.clearable, PropTypes.string),
-    clearable: PropTypes.bool,
     dataTest: PropTypes.string,
-    disabled: PropTypes.bool,
     inputMaxHeight: PropTypes.string,
-    options: PropTypes.node,
-    placeholder: PropTypes.string,
     prefix: PropTypes.string,
-    selected: PropTypes.string,
-    onChange: PropTypes.func,
+    value: PropTypes.string,
 }
 
 export { Input }
