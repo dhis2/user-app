8751742401578fd00d10353146869a6f26d005bb
Médi-Rémi Hashim <4295266+mediremi@users.noreply.github.com>
2021-10-19 15:00:31 +0100

    test: setup cypress (#839)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     extends: [config.eslintReact],
+    globals: {
+        cy: 'readonly',
+        Cypress: 'readonly',
+    },
     rules: {
         'react/display-name': 'off',
     },
