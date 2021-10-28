ab83d8b7d2a31e12f32ac822482aa55d54eb2d99
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-10-28 11:26:01 +0200

    chore(homepage cypress test): fix tests after sidenav refactor

--- cypress/integration/homepage/show-action-tiles/index.js
+++ cypress/integration/homepage/show-action-tiles/index.js
@@ -7,36 +7,36 @@ Given('the user-manager is on the homepage', () => {
 When(
     'the user-manager clicks on the add user group icon of the user group tile',
     () => {
-        cy.get('.left-bar + div > div:nth-child(3) button:first-child').click()
+        cy.findByRole('nav').find('+ div > div:nth-child(3) button:first-child').click()
     }
 )
 
 When('the user-manager clicks on the add user icon of the user tile', () => {
-    cy.get('.left-bar + div > div:nth-child(1) button:first-child').click()
+    cy.findByRole('nav').find('+ div > div:nth-child(1) button:first-child').click()
 })
 
 When(
     'the user-manager clicks on the add user role icon of the user role tile',
     () => {
-        cy.get('.left-bar + div > div:nth-child(2) button:first-child').click()
+        cy.findByRole('nav').find('+ div > div:nth-child(2) button:first-child').click()
     }
 )
 
 When(
     'the user-manager clicks on the user group list icon of the user group tile',
     () => {
-        cy.get('.left-bar + div > div:nth-child(3) button:first-child').click()
+        cy.findByRole('nav').find('+ div > div:nth-child(3) button:first-child').click()
     }
 )
 
 When('the user-manager clicks on the user list icon of the user tile', () => {
-    cy.get('.left-bar + div > div:nth-child(1) button:first-child').click()
+    cy.findByRole('nav').find('+ div > div:nth-child(1) button:first-child').click()
 })
 
 When(
     'the user-manager clicks on the user role list icon of the user role tile',
     () => {
-        cy.get('.left-bar + div > div:nth-child(2) button:first-child').click()
+        cy.findByRole('nav').find('+ div > div:nth-child(2) button:first-child').click()
     }
 )
 
