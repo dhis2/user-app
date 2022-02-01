9cbfef14b383c26ee88ffa316e07f8c80faca3b2
Médi-Rémi Hashim <mediremi@gmail.com>
2022-02-01 18:27:55 +0000

    feat: finish RoleForm

--- src/components/RoleForm/getRoleData.js
+++ src/components/RoleForm/getRoleData.js
@@ -1,1 +1,24 @@
-// TODO
+export const getRoleData = ({ values, role }) => {
+    const {
+        name,
+        description,
+        metadataAuthorities,
+        appAuthorities,
+        trackerAuthorities,
+        importExportAuthorities,
+        systemAuthorities,
+    } = values
+
+    return {
+        id: role?.id,
+        name,
+        description,
+        authorities: [
+            ...metadataAuthorities,
+            ...appAuthorities,
+            ...trackerAuthorities,
+            ...importExportAuthorities,
+            ...systemAuthorities,
+        ],
+    }
+}
