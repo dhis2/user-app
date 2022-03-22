34ade531be08b0d284fb9db7ba9e9a29bf37b08a
Médi-Rémi Hashim <mediremi@gmail.com>
2022-03-22 16:45:55 +0000

    refactor: migrate SectionLoader away from class component

--- src/components/SectionLoader.js
+++ src/components/SectionLoader.js
@@ -1,7 +1,8 @@
+import { useD2 } from '@dhis2/app-runtime-adapter-d2'
 import i18n from '@dhis2/d2-i18n'
 import { CenteredContent, CircularLoader, NoticeBox } from '@dhis2/ui'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Switch, Route, withRouter } from 'react-router-dom'
 import { initCurrentUser } from '../actions'
@@ -14,139 +15,131 @@ import SideNav from './SideNav'
  * This component prepares the user-app sections and routes based on the current user's authorities
  * The sections will be rendered in the SideNav and the routes within a react-router Switch component
  */
-class SectionLoader extends Component {
-    componentDidMount() {
-        const { initCurrentUser, currentUser } = this.props
-        if (!currentUser) {
-            initCurrentUser()
-        } else {
-            this.setRouteConfig(currentUser)
-        }
+
+const userHasAuthorities = ({ entity, currentUser, d2 }) => {
+    const { entityType } = entity
+    if (!entityType) {
+        return true
     }
 
-    componentWillReceiveProps({ currentUser, location: { pathname } }) {
-        this.setRouteConfig(currentUser)
-
-        // Navigate home if the current user has edited a setting that restricts
-        // him from an active section. I.e. adjusting his own user role
-        // so he cannot edit user roles anymore
-        if (
-            currentUser &&
-                currentUser !== this.props.currentUser &&
-                pathname !== '/' &&
-                !this.pathHasAvailableSection(pathname)
-        ) {
-            navigateTo('/')
-        }
+    // In case of error `currentUser` is a string containing the error message
+    // TODO: modify `const routeConfig = !currentUser` line to `const routeConfig = typeof currentUser === 'object'`
+    // to remove this check
+    if (typeof currentUser === 'string') {
+        return false
     }
 
-    pathHasAvailableSection(pathname) {
-        if (!this.routeConfig) {
-            return false
-        }
-
-        const { sections } = this.routeConfig
-        return Boolean(
-            sections &&
-                sections.find(section =>
-                    pathname.includes(section.path.split(':')[0])
-                )
-        )
-    }
-
-    setRouteConfig(currentUser) {
-        // Only show menu items for which the user has either the "add" or "delete" authority
-        this.routeConfig = !currentUser
-            ? null
-            : getRouteConfig().reduce(
-                (routeConfig, configItem) => {
-                    const { routes, sections } = routeConfig
-                    if (this.userHasAuthorities(configItem, currentUser)) {
-                        routes.push(configItem)
-                        if (configItem.label) {
-                            sections.push(configItem)
-                        }
-                    }
-                    return routeConfig
-                },
-                { routes: [], sections: [] }
-            )
-    }
-
-    userHasAuthorities({ entityType }, currentUser) {
-        if (!entityType) {
-            return true
-        }
-
-        // In case of error `currentUser` is a string containing the error message
-        if (typeof currentUser === 'string') {
-            return false
-        }
-
-        const { models } = this.context.d2
-        const canCreate = currentUser.canCreate(models[entityType])
-        const canDelete = currentUser.canDelete(models[entityType])
-        return canCreate || canDelete
-    }
-
-    render() {
-        const { currentUser } = this.props
-
-        if (!currentUser) {
-            return (
-                <CenteredContent>
-                    <CircularLoader />
-                </CenteredContent>
-            )
-        }
-
-        if (typeof currentUser === 'string') {
-            const errorMessage = currentUser
-            return (
-                <NoticeBox
-                    error
-                    title={i18n.t(
-                        'There was an error loading the current user'
-                    )}
-                    className={styles.noticeBox}
-                >
-                    {errorMessage}
-                </NoticeBox>
-            )
-        }
-
-        const { routes, sections } = this.routeConfig
-
-        if (sections && sections.length === 0) {
-            return (
-                <NoticeBox
-                    error
-                    title={i18n.t(
-                        'You do not have authorities to see users, user roles or user groups'
-                    )}
-                    className={styles.noticeBox}
-                >
-                </NoticeBox>
-            )
-        }
-
-        return (
-            <main className={styles.container}>
-                <SideNav key="sidenav" sections={sections} />
-                <div className={styles.content}>
-                    <Switch key="routeswitch">
-                        {routes.map(section => (
-                            <Route key={section.key} exact strict {...section} />
-                        ))}
-                    </Switch>
-                </div>
-            </main>
-        )
-    }
+    const { models } = d2
+    const canCreate = currentUser.canCreate(models[entityType])
+    const canDelete = currentUser.canDelete(models[entityType])
+    return canCreate || canDelete
 }
 
-SectionLoader.contextTypes = {
-    d2: PropTypes.object,
+const SectionLoader = ({
+    initCurrentUser,
+    currentUser,
+    location: { pathname },
+}) => {
+    const { d2 } = useD2()
+
+    useEffect(() => {
+        if (!currentUser) {
+            initCurrentUser()
+        }
+    }, [])
+
+    useEffect(() => {
+        if (!currentUser || typeof currentUser === 'string') {
+            return
+        }
+
+        // Navigate home if the current user has edited a setting that restricts
+        // them from an active section (i.e. adjusting their own user role so
+        // they cannot edit user roles anymore)
+        if (pathname !== '/' && !pathHasAvailableSection(pathname)) {
+            navigateTo('/')
+        }
+    }, [currentUser, pathname])
+
+    // Only show menu items for which the user has either the "add" or "delete" authority
+    const routeConfig = !currentUser
+        ? null
+        : getRouteConfig().reduce(
+              (routeConfig, configItem) => {
+                  const { routes, sections } = routeConfig
+                  if (
+                      userHasAuthorities({
+                          entity: configItem,
+                          currentUser,
+                          d2,
+                      })
+                  ) {
+                      routes.push(configItem)
+                      if (configItem.label) {
+                          sections.push(configItem)
+                      }
+                  }
+                  return routeConfig
+              },
+              { routes: [], sections: [] }
+          )
+    const pathHasAvailableSection = pathname => {
+        if (!routeConfig) {
+            return false
+        }
+
+        return routeConfig.sections.some(section =>
+            pathname.includes(section.path.split(':')[0])
+        )
+    }
+
+    if (!currentUser) {
+        return (
+            <CenteredContent>
+                <CircularLoader />
+            </CenteredContent>
+        )
+    }
+
+    if (typeof currentUser === 'string') {
+        const errorMessage = currentUser
+        return (
+            <NoticeBox
+                error
+                title={i18n.t('There was an error loading the current user')}
+                className={styles.noticeBox}
+            >
+                {errorMessage}
+            </NoticeBox>
+        )
+    }
+
+    const { routes, sections } = routeConfig
+
+    if (sections && sections.length === 0) {
+        return (
+            <NoticeBox
+                error
+                title={i18n.t(
+                    'You do not have authorities to see users, user roles or user groups'
+                )}
+                className={styles.noticeBox}
+            ></NoticeBox>
+        )
+    }
+
+    return (
+        <main className={styles.container}>
+            <SideNav key="sidenav" sections={sections} />
+            <div className={styles.content}>
+                <Switch key="routeswitch">
+                    {routes.map(section => (
+                        <Route key={section.key} exact strict {...section} />
+                    ))}
+                </Switch>
+            </div>
+        </main>
+    )
 }
 
 SectionLoader.propTypes = {
