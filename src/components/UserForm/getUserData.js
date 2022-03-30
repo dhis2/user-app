02d1d8daec8d98bd2703ac092efd5764b830d683
Médi-Rémi Hashim <mediremi@gmail.com>
2022-03-30 12:55:11 +0100

    refactor(user-form): don't use userCredentials field when creating user

--- src/components/UserForm/getUserData.js
+++ src/components/UserForm/getUserData.js
@@ -50,33 +50,27 @@ export const getUserData = ({
         // object.
         ...user,
 
-        userCredentials: {
-            id: user?.userCredentials?.id,
-            userInfo: user ? { id: user.id } : undefined,
-
-            username,
-            disabled,
-            password:
-                !inviteUser && !externalAuth && (!user || changePassword)
-                    ? password
-                    : undefined,
-            // See https://jira.dhis2.org/browse/DHIS2-10569
-            accountExpiry:
-                typeof accountExpiry === 'string' && accountExpiry !== ''
-                    ? accountExpiry
-                    : undefined,
-            openId,
-            ldapId,
-            externalAuth,
-            userRoles: wrapIds(userRoles),
-            // Dimension constraints are combined into a single input
-            // component, but need to be stored separately
-            catDimensionConstraints: constraintsForType('CATEGORY'),
-            cogsDimensionConstraints: constraintsForType(
-                'CATEGORY_OPTION_GROUP_SET'
-            ),
-        },
-
+        username,
+        disabled,
+        password:
+            !inviteUser && !externalAuth && (!user || changePassword)
+                ? password
+                : undefined,
+        // See https://jira.dhis2.org/browse/DHIS2-10569
+        accountExpiry:
+            typeof accountExpiry === 'string' && accountExpiry !== ''
+                ? accountExpiry
+                : undefined,
+        openId,
+        ldapId,
+        externalAuth,
+        userRoles: wrapIds(userRoles),
+        // Dimension constraints are combined into a single input
+        // component, but need to be stored separately
+        catDimensionConstraints: constraintsForType('CATEGORY'),
+        cogsDimensionConstraints: constraintsForType(
+            'CATEGORY_OPTION_GROUP_SET'
+        ),
         email,
         firstName,
         surname,
