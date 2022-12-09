e8537f6559aa9db88cc236c9551af81e5056d1d6
Joseph John Aas Cooper <joe@dhis2.org>
2022-12-09 08:40:27 +0100

    chore: update emptyTableInfo component

--- src/components/EmptyTableInfo.js
+++ src/components/EmptyTableInfo.js
@@ -3,21 +3,19 @@ import React from 'react'
 import styles from './EmptyTableInfo.module.css'
 
 const EmptyTableInfo = ({ action, header, icon, text }) => (
-    <>
-        <div className={styles.wrapper} data-test="data-test-empty-table-info">
-            <div className={styles.icon}>{icon}</div>
-            <p className={styles.header}>{header}</p>
-            <p className={styles.text}>{text}</p>
-            <div className={styles.action}>{action}</div>
-        </div>
-    </>
+    <div className={styles.wrapper} data-test="data-test-empty-table-info">
+        <div className={styles.icon}>{icon}</div>
+        <p className={styles.header}>{header}</p>
+        <p className={styles.text}>{text}</p>
+        <div className={styles.action}>{action}</div>
+    </div>
 )
 
 EmptyTableInfo.propTypes = {
     action: PropTypes.node,
-    header: PropTypes.node,
+    header: PropTypes.string,
     icon: PropTypes.node,
-    text: PropTypes.node,
+    text: PropTypes.string,
 }
 
 export default EmptyTableInfo
