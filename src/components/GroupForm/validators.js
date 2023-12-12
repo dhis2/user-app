4b5d6a0a5490f5a19364990ab9e95a3724909a4b
Thomas Zemp <tom@dhis2.org>
2023-12-12 15:02:49 +0100

    fix: update user group form [DHIS2-12294]

--- src/components/GroupForm/validators.js
+++ src/components/GroupForm/validators.js
@@ -1,3 +1,4 @@
+import { useDataEngine } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import pDebounce from 'p-debounce'
 import { useValidator } from '../../hooks/useValidator'
@@ -5,9 +6,9 @@ import { useValidator } from '../../hooks/useValidator'
 const DEBOUNCE_DELAY_MS = 350
 
 export const useDebouncedUniqueGroupNameValidator = ({
-    engine,
     groupName: currentGroupName,
 }) => {
+    const engine = useDataEngine()
     const findGroupByName = pDebounce(async groupName => {
         const {
             groups: { userGroups: groups },
@@ -33,6 +34,7 @@ export const useDebouncedUniqueGroupNameValidator = ({
                 return i18n.t('Name is already taken')
             }
         } catch (error) {
+            console.log(error)
             return i18n.t(
                 'There was a problem whilst checking the availability of this group name'
             )
@@ -42,9 +44,9 @@ export const useDebouncedUniqueGroupNameValidator = ({
 }
 
 export const useDebouncedUniqueGroupCodeValidator = ({
-    engine,
     groupCode: currentGroupCode,
 }) => {
+    const engine = useDataEngine()
     const findGroupByCode = pDebounce(async groupCode => {
         const {
             groups: { userGroups: groups },
