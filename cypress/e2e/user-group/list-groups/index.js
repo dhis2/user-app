9f8597eabc1190b7078b10e47195344b259c4f76
Diana Nanyanzi <diana@dhis2.org>
2024-06-15 22:15:42 +0300

    chore: upgrade cypress setup in the user-app project
    Changes:
    - upgrade cypress to version 13.0
    - upgrade @dhis2/cypress-commands to 10.0.6
    - upgrade @dhis2/cypress-plugins to 10.0.6
    - replace cypress.json with new config file cypress.config.js
    - replace cypress-cucumber-preprocessor package with @badeball/cypress-cucumber-preprocessor
    - rename cypress/integration folder to cypress/e2e
    - rename cypress/support/index.js to cypress/support/e2e.js

--- cypress/e2e/user-group/list-groups/index.js
+++ cypress/e2e/user-group/list-groups/index.js
@@ -1,16 +1,16 @@
 import '../common.js'
-import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps'
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor'
 
-Then('the user-manager should see the first page of all users', () => {
+Then('the user-manager should see the first page of all user groups', () => {
     cy.getWithDataTest('{dhis2-uiwidgets-pagination-summary}').contains(
         'Page 1'
     )
 })
 
 Given(
-    'enough users exist to show a second page and the user-manager navigated to the user list view',
+    'enough user groups exist to show a second page the user-manager navigated to the user group list view',
     () => {
-        cy.visit('/#/users?pageSize=5')
+        cy.visit('/#/user-groups?pageSize=5')
         cy.getWithDataTest('{dhis2-uicore-circularloader}').should('not.exist')
     }
 )
@@ -21,7 +21,7 @@ When('the user-manager opens the next page', () => {
     cy.getWithDataTest('{dhis2-uiwidgets-pagination-page-next}').click()
 })
 
-Then('the user-manager should see the second page of all users', () => {
+Then('the user-manager should see the second page of all user groups', () => {
     cy.getWithDataTest('{dhis2-uiwidgets-pagination-summary}').contains(
         'Page 2'
     )
