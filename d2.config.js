165951e14ee845ed0c1999dce9f18a064d30eca6
Austin McGee <947888+amcgee@users.noreply.github.com>
2024-06-18 14:49:14 +0200

    chore: update app label to match hardcoded name in core

--- d2.config.js
+++ d2.config.js
@@ -2,7 +2,7 @@ const config = {
     id: '1887e1f8-3b62-42a3-8383-975996e0870f',
     type: 'app',
     coreApp: true,
-    title: 'User management',
+    title: 'Users',
     name: 'user',
     description:
         'An application for managing users, user-groups and user-roles',
