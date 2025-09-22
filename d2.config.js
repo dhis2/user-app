867d07bf0197e2d7dc1a7f66e79a55cfe292e4ee
Diana Nanyanzi <31903212+d-rita@users.noreply.github.com>
2025-09-22 15:23:55 +0200

    feat: add shortcuts to the command palette (#1557)
    * chore: update cli-app-scripts
    
    * feat: add user app shortcuts

--- d2.config.js
+++ d2.config.js
@@ -1,3 +1,4 @@
+/** @type {import('@dhis2/cli-app-scripts').D2Config} */
 const config = {
     id: '1887e1f8-3b62-42a3-8383-975996e0870f',
     type: 'app',
@@ -10,6 +11,32 @@ const config = {
         app: './src/AppWrapper.jsx',
     },
     minDHIS2Version: '2.41',
+    shortcuts: [
+        {
+            name: 'Users',
+            url: '#/users',
+        },
+        {
+            name: 'New user',
+            url: '#/users/new',
+        },
+        {
+            name: 'User roles',
+            url: '#/user-roles',
+        },
+        {
+            name: 'New user role',
+            url: '#/user-roles/new',
+        },
+        {
+            name: 'User groups',
+            url: '#/user-groups',
+        },
+        {
+            name: 'New user group',
+            url: '#/user-groups/new',
+        },
+    ],
 }
 
 module.exports = config
